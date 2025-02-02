09d4a3d2e0d2b18c0c044472e4bcf3c229a70f0f
olubiyi babajide <olubiyibabajide@gmail.com>
2025-02-02 01:05:52 +0100

    top bar done

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Homepage from './pages/home/Homepage'
+
+export default function App() {
+  return (
+    <Homepage/>
+  )
+}
---
+++ src/pages/home/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Topbar from '../../components/topbar/Topbar'
+
+
+export default function Homepage() {
+  return (
+    <Topbar/>
+  )
+}
