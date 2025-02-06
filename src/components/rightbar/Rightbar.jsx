064ab7c0f5d7c8dd5168b6c750a1aad641aa0b76
olubiyi babajide <olubiyibabajide@gmail.com>
2025-02-06 23:43:38 +0100

    right bar done(layout completed

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,8 +1,88 @@
 import React from 'react'
 import "./rightbar.css"
+import Gift from "../../../public/Posts/gift.jpg"
+import Ads from "../../../public/Posts/ads.jpg"
+import  Friend from "../../../public/assest/2.jpg"
 
 export default function Rightbar() {
   return (
-    <div className='rightBar'>Rightbar</div>
+    <div className='rightBar'>
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src={Gift} alt="" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img className='rightbarAds' src={Ads} alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className='rightbarFriendList'>
+          <li className='rightbarFriend'>
+            <div className="rightbarProfileImgContainer">
+              <img className='rightProfileImg' src={Friend} alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">
+              John Carter
+            </span>
+          </li>
+        </ul>
+        <ul className='rightbarFriendList'>
+          <li className='rightbarFriend'>
+            <div className="rightbarProfileImgContainer">
+              <img className='rightProfileImg' src={Friend} alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">
+              John Carter
+            </span>
+          </li>
+        </ul>
+        <ul className='rightbarFriendList'>
+          <li className='rightbarFriend'>
+            <div className="rightbarProfileImgContainer">
+              <img className='rightProfileImg' src={Friend} alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">
+              John Carter
+            </span>
+          </li>
+        </ul>
+        <ul className='rightbarFriendList'>
+          <li className='rightbarFriend'>
+            <div className="rightbarProfileImgContainer">
+              <img className='rightProfileImg' src={Friend} alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">
+              John Carter
+            </span>
+          </li>
+        </ul>
+        <ul className='rightbarFriendList'>
+          <li className='rightbarFriend'>
+            <div className="rightbarProfileImgContainer">
+              <img className='rightProfileImg' src={Friend} alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">
+              John Carter
+            </span>
+          </li>
+        </ul>
+        <ul className='rightbarFriendList'>
+          <li className='rightbarFriend'>
+            <div className="rightbarProfileImgContainer">
+              <img className='rightProfileImg' src={Friend} alt="" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">
+              John Carter
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
